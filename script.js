09b1af98156806c079a65c043e3c253be9b21cda
abhi1302-js <69789359+abhi1302-js@users.noreply.github.com>
2020-10-07 13:41:22 +0400

    commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
  function prompt() {
     alert("I AGREE TO ALL THE TERMS AND CONDITIONS OF THIS WEBSITE");
+    location.replace("loadingpage.html");
     location.replace("mainpage.html");
    
 }
